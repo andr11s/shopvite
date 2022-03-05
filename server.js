56443c31dda21df2d9a489f79992a1f5764b50d9
Alvis herrera <alvisandres1@gmail.com>
2022-03-04 22:00:25 -0500

    config file

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname, './dist/shopviteV2')));
 
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist" })
+  res.sendFile("index.html", { root: "dist/shopviteV2/" })
 );
  
 app.listen(process.env.PORT || 8080);
