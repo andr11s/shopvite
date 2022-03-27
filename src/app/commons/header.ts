494f0332f8b3d76e4bfb8839c2db52c7652ef913
Alvis herrera <alvisandres1@gmail.com>
2022-03-26 23:37:47 -0500

    feact(header) add header tap bar

--- src/app/commons/header.ts
+++ src/app/commons/header.ts
@@ -7,20 +7,32 @@ export function Header() {
   $header.classList.add('header');
   $header.classList.add('container');
   $header.innerHTML = `
-    <div class="row">
-           <div class="col-md-6">
-           <img src=${imageLogo} alt="aaaa"> 
-          </div>
-          <div class="col-md-6"> 
-              <nav class="header-container-menu"> 
-                      <a class="is-active" href="#/">HOME</a>
-                      <a href="#">SNEAKERS</a>
-                      <a href="#">BELT</a> 
-                      <a href="#">CONTACT</a> 
-              </nav>  
-          </div>  
+  <header class="header">
+  <div class="header__menu">
+    <nav class="menu">
+      <a href="#" class="menu-item">
+        <span class="material-icons"><i class="fa-solid fa-house"></i></span>
+        <span class="menu-item-label">Home</span>
+      </a>
+      <a href="#" class="menu-item">
+        <span class="material-icons"><i class="fa-solid fa-magnifying-glass"></i> </span>
+        <span class="menu-item-label">Explorer</span>
+      </a>
+      <a href="#" class="menu-item">
+        <span class="material-icons"><i class="fa-solid fa-cart-shopping"></i></span>
+        <span class="menu-item-label">Cart</span>
+      </a>
+      <a href="#" class="menu-item">
+        <span class="material-icons"><i class="fa-solid fa-tag"></i></span>
+        <span class="menu-item-label">Offer</span>
+      </a>
+      <a href="#" class="menu-item">
+        <span class="material-icons"><i class="fa-solid fa-user"></i></span>
+        <span class="menu-item-label">Account</span>
+      </a>
+    </nav>
   </div>
-      `;
+</header>`;
 
   return $header;
 }
