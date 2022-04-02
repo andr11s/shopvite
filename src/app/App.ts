02da16ff4f9d439952ac524e9241a7f233db15a6
Alvis herrera <alvisandres1@gmail.com>
2022-04-01 22:52:03 -0500

    feact(header): update loading header

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,13 +1,21 @@
-import { Header } from "./commons/header";
-import MenuResponsive from "./commons/menu";
-import { Main } from "./components/main";
+import { Header } from './commons/header';
+import MenuResponsive from './commons/menu';
+import { Main } from './components/main';
 const d = document;
 
 export async function App() {
-    let $app = d.getElementById('app');
+  let $app = d.getElementById('app');
+  let $loading = d.getElementById('.loader-page');
 
-    $app?.appendChild(Header());
-    $app?.appendChild(Main());
+  $app?.appendChild(Header());
+  $app?.appendChild(Main());
+
+  setTimeout(() => {
+    $loading?.style.setProperty('visibility', 'hidden');
+    $loading?.style.setProperty('opacity', '0');
+
+    console.log('yaaaa');
+  }, 2000);
 }
 
-d.addEventListener("click", (e: MouseEvent) => MenuResponsive(e));
+d.addEventListener('click', (e: MouseEvent) => MenuResponsive(e));
